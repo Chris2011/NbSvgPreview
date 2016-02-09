c8902417dcce490b04af127bb0a3f11d1422754a
Chris <chrl@spreadshirt.net>
2016-02-09 13:42:04 +0100

    Refactored code.

--- src/main/java/org/chrisle/netbeans/plugins/nbsvgpreview/NbSvgPreviewVisualElement.java
+++ src/main/java/org/chrisle/netbeans/plugins/nbsvgpreview/NbSvgPreviewVisualElement.java
@@ -3,39 +3,135 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package org.chrisle.netbeans.plugins.nbsvgpreview;
 
+import java.awt.EventQueue;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.io.IOException;
+import java.util.EnumSet;
+import java.util.Set;
 import javax.swing.Action;
 import javax.swing.JComponent;
 import javax.swing.JPanel;
 import javax.swing.JToolBar;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+import javax.swing.text.StyledDocument;
 import org.netbeans.core.spi.multiview.CloseOperationState;
 import org.netbeans.core.spi.multiview.MultiViewElement;
 import org.netbeans.core.spi.multiview.MultiViewElementCallback;
+import org.openide.awt.StatusDisplayer;
 import org.openide.awt.UndoRedo;
+import org.openide.filesystems.FileObject;
+import org.openide.loaders.DataObject;
 import org.openide.util.Lookup;
 import org.openide.util.NbBundle.Messages;
+import org.openide.util.RequestProcessor;
 import org.openide.windows.TopComponent;
 
 @MultiViewElement.Registration(
-    displayName = "#LBL_NbSvgPreview_VISUAL",
-    iconBase = "org/chrisle/netbeans/plugins/nbsvgpreview/svg-icon.png",
-    mimeType = "text/svg+xml",
-    persistenceType = TopComponent.PERSISTENCE_NEVER,
-    preferredID = "NbSvgPreviewVisual",
-    position = 2000
+        displayName = "#LBL_NbSvgPreview_VISUAL",
+        iconBase = "org/chrisle/netbeans/plugins/nbsvgpreview/svg-icon.png",
+        mimeType = "text/svg+xml",
+        persistenceType = TopComponent.PERSISTENCE_NEVER,
+        preferredID = "NbSvgPreviewVisual",
+        position = 2000
 )
 @Messages("LBL_NbSvgPreview_VISUAL=Preview")
 public final class NbSvgPreviewVisualElement extends JPanel implements MultiViewElement {
-    private NbSvgPreviewDataObject obj;
+
+    private static final RequestProcessor RP = new RequestProcessor(NbSvgPreviewVisualElement.class);
+
+    private final Lookup context;
+
+    private final FileObject sourceFile;
+
+    private StyledDocument sourceDoc;
+    
+    private final Object lock = new Object();
+    private final IHtmlView htmlView;
+
+    private final DocumentListener sourceDocListener;
+    
+    private final PropertyChangeListener htmlViewListener;
+
+    private final RequestProcessor.Task updateTask;
+
+//    private NbSvgPreviewDataObject obj;
     private JToolBar toolbar = new JToolBar();
     private transient MultiViewElementCallback callback;
 
-    public NbSvgPreviewVisualElement(Lookup lkp) {
-        obj = lkp.lookup(NbSvgPreviewDataObject.class);
-        assert obj != null;
-        initComponents();
+    public NbSvgPreviewVisualElement(Lookup context) {
+        this.context = context;
+        this.callback = null;
+        this.sourceDoc = null;
+        this.sourceDocListener = new DocumentHandler();
+        this.updateTask = RP.create(new Runnable() {
+            @Override
+            public void run() {
+                doUpdatePreview();
+            }
+        });
+
+        DataObject dataObj = context.lookup(DataObject.class);
+        if (dataObj != null) {
+            sourceFile = dataObj.getPrimaryFile();
+        } else {
+            sourceFile = null;
+        }
+
+        htmlView = new HtmlViewFactory().createHtmlView();
+        htmlViewListener = new PropertyChangeHandler();
+
+//        toolbar = new JToolBar();
+//        toolbar.setFloatable(false);
+//        toolbar.addSeparator();
+//        toolbar.add(new PreviewExternalAction(context));
+
+//        obj = lkp.lookup(NbSvgPreviewDataObject.class);
+//        assert obj != null;
+//        initComponents();
+    }
+
+    private void updatePreview() {
+        updateTask.schedule(300);
+    }
+
+    private void doUpdatePreview() {
+        final StyledDocument localSourceDoc = getSourceDocument();
+        if (localSourceDoc != null) {
+            final String previewText = renderPreview();
+            EventQueue.invokeLater(new Runnable() {
+                @Override
+                public void run() {
+                    htmlView.setContent(previewText);
+                }
+            });
+        }
+    }
+
+    private String renderPreview() {
+        IRenderable renderable = context.lookup(IRenderable.class);
+        String previewText;
+        try {
+            Set<RenderOption> renderOptions = EnumSet.of(
+                    RenderOption.PREFER_EDITOR,
+                    RenderOption.RESOLVE_IMAGE_URLS);
+            if (!htmlView.isHtmlFullySupported()) {
+                renderOptions.add(RenderOption.SWING_COMPATIBLE);
+            }
+            previewText = renderable.renderAsHtml(renderOptions);
+        } catch (IOException ex) {
+            previewText = "Preview rendering failed: " + ex.getMessage();
+        }
+        return previewText;
+    }
+
+    private StyledDocument getSourceDocument() {
+        synchronized (lock) {
+            return sourceDoc;
+        }
     }
 
     @Override
@@ -43,10 +139,10 @@ public final class NbSvgPreviewVisualElement extends JPanel implements MultiView
         return "NbSvgPreviewVisualElement";
     }
 
-    /** This method is called from within the constructor to
-     * initialize the form.
-     * WARNING: Do NOT modify this code. The content of this method is
-     * always regenerated by the Form Editor.
+    /**
+     * This method is called from within the constructor to initialize the form.
+     * WARNING: Do NOT modify this code. The content of this method is always
+     * regenerated by the Form Editor.
      */
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
@@ -66,8 +162,6 @@ public final class NbSvgPreviewVisualElement extends JPanel implements MultiView
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     // End of variables declaration//GEN-END:variables
-
-
     @Override
     public JComponent getVisualRepresentation() {
         return this;
@@ -85,7 +179,7 @@ public final class NbSvgPreviewVisualElement extends JPanel implements MultiView
 
     @Override
     public Lookup getLookup() {
-        return obj.getLookup();
+        return context;
     }
 
     @Override
@@ -126,4 +220,59 @@ public final class NbSvgPreviewVisualElement extends JPanel implements MultiView
     public CloseOperationState canCloseElement() {
         return CloseOperationState.STATE_OK;
     }
+
+    private class DocumentHandler implements DocumentListener {
+
+        @Override
+        public void insertUpdate(DocumentEvent e) {
+            updatePreview();
+        }
+
+        @Override
+        public void removeUpdate(DocumentEvent e) {
+            updatePreview();
+        }
+
+        @Override
+        public void changedUpdate(DocumentEvent e) {
+            updatePreview();
+        }
+    }
+
+    private class PropertyChangeHandler implements PropertyChangeListener {
+
+        @Override
+        public void propertyChange(PropertyChangeEvent evt) {
+            if (evt.getPropertyName() == null
+                    || IHtmlView.PROP_STATUS_MESSAGE.equals(evt.getPropertyName())) {
+                String statusMessage = htmlView.getStatusMessage();
+                StatusDisplayer.getDefault().setStatusText(statusMessage);
+            }
+        }
+    }
+    
+//     @NbBundle.Messages({
+//        "NAME_PreviewExternalAction=Preview in external browser"
+//    })
+//    public static class PreviewExternalAction extends AbstractAction {
+//        private static final long serialVersionUID = 1L;
+//
+//        @StaticResource
+//        private static final String ICON_PATH = "flow/netbeans/markdown/resources/action-view.png";
+//
+//        private final Lookup context;
+//
+//        public PreviewExternalAction(Lookup context) {
+//            super(Bundle.NAME_PreviewExternalAction(), ImageUtilities.loadImageIcon(ICON_PATH, false));
+//            putValue(Action.SHORT_DESCRIPTION, Bundle.NAME_PreviewExternalAction());
+//            this.context = context;
+//        }
+//
+//        @Override
+//        public void actionPerformed(ActionEvent e) {
+//            NbSvgPreviewDataObject dataObject = context.lookup(NbSvgPreviewDataObject.class);
+//            MarkdownViewHtmlAction viewAction = new MarkdownViewHtmlAction(dataObject);
+//            viewAction.actionPerformed(null);
+//        }
+//    }
 }
